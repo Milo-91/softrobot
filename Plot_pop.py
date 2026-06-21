e5d941d94ed5c22d07bb0f9f0a9ca542b0b97047
Milo-91 <milolin9112@gmail.com>
2026-06-21 22:47:42 +0900

    add top 10 population distribution graph

--- Plot_pop.py
+++ Plot_pop.py
@@ -8,14 +8,16 @@ if __name__ == '__main__':
     ]
     
     for file in population_files_list:
-        plt.figure(figsize = (18, 7.3))
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize = (18, 7.3))
         plt.rcParams.update({'font.size': 14})
 
         fitness_history = []
+        top_10_fitness_history = []
         with open('log/' + file + '_population_record.jsonl') as f:
             for line in f:
                 data = json.loads(line)
                 fitness_history.append(data["fitness"])
+                top_10_fitness_history.append(data["fitness"][:max(int(len(top_10_fitness_history)*0.1), 1)])
         all_fitness = np.concatenate(fitness_history)
         f_max = all_fitness.max()
         f_min = all_fitness.min()
@@ -26,16 +28,16 @@ if __name__ == '__main__':
             11
         )
 
+        # all population heatmap
         heatmap = []
         for pop in fitness_history:
             hist, _ = np.histogram(pop, bins=bins)
             print(hist)
-            
             hist = hist / len(pop) # change into percentage
-
             heatmap.append(hist)
+
         heatmap = np.array(heatmap)
-        img = plt.imshow(
+        img = ax[0].imshow(
             heatmap.T,
             aspect = 'auto',
             origin = 'lower',
@@ -43,8 +45,42 @@ if __name__ == '__main__':
             cmap = 'plasma'
         )    
         
-        plt.title("Population distribution")
-        plt.xlabel("Generation")
-        plt.ylabel("Fitness")
-        plt.colorbar(img)
+        ax[0].set_title("Population distribution")
+        ax[0].set_xlabel("Generation")
+        ax[0].set_ylabel("Fitness")
+        fig.colorbar(img, ax=ax[0])
+
+        
+        # top 10 heatmap
+        top_10_all_fitness = np.concatenate(top_10_fitness_history)
+        top_10_f_max = top_10_all_fitness.max()
+        top_10_f_min = top_10_all_fitness.min()
+
+        top_10_bins = np.linspace(
+            top_10_f_min,
+            top_10_f_max,
+            11
+        )
+
+        top_10_heatmap = []
+        for pop in top_10_fitness_history:
+            hist, _ = np.histogram(pop, bins=top_10_bins)
+            print(hist)
+            hist = hist / len(pop) # change into percentage
+            top_10_heatmap.append(hist)
+
+        top_10_heatmap = np.array(top_10_heatmap)
+        top_10_img = ax[1].imshow(
+            top_10_heatmap.T,
+            aspect = 'auto',
+            origin = 'lower',
+            extent = (0, len(top_10_fitness_history), top_10_f_min, top_10_f_max),
+            cmap = 'plasma'
+        )    
+        
+        ax[1].set_title("Top 10 distribution")
+        ax[1].set_xlabel("Generation")
+        ax[1].set_ylabel("Fitness")
+        fig.colorbar(top_10_img, ax=ax[1])
+
         plt.show()
