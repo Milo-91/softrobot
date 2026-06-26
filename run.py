c111a1078e5d0f0e316f850b8815aec0106b65e4
Milo-91 <milolin9112@gmail.com>
2026-06-26 22:57:37 +0900

    draw simulation results

--- run.py
+++ run.py
@@ -5,10 +5,11 @@ if __name__ == '__main__':
     algorithm = "GA"
     local_search = None
     sim_step = "400"
-    evo_step = "100"
-    popsize = "10"
-    tasks = ["ObstacleTraverser-v0T.json", "ObstacleTraverser-v1T.json", "Climber-v0T.json"]
-    logdir = f"log/20260623/{popsize}/"
+    evo_step = "10000"
+    popsize = "100"
+    # tasks = ["ObstacleTraverser-v0T.json", "ObstacleTraverser-v1T.json", "Climber-v0T.json"]
+    tasks = ["ObstacleTraverser-v1T.json"]
+    logdir = f"log/20260626/popsize{popsize}/"
     numprocs = "7" #TODO change it on server
     
     for task in tasks:
@@ -16,14 +17,12 @@ if __name__ == '__main__':
         while rho < 1: 
             tau = 0.5
             while tau <= 32:
-                record_files = []
                 sub_logdir = f'{logdir}/{task[:-5]}/r{rho}_t{tau}/'
                 for i in range(5):
                     if local_search == None:
                         filename = f'{sub_logdir}{task[:-5]}_{algorithm}_{popsize}_{time.strftime("%m%d%H%M")}_{rho}_{tau}'
                     else:
                         filename = f'{sub_logdir}{task[:-5]}_{algorithm}_{local_search}_{popsize}_{time.strftime("%m%d%H%M")}_{rho}_{tau}'
-                    record_files.append(filename)
                     print(f'[{i}/5]')
                     subprocess.run([
                         "python",
@@ -42,6 +41,31 @@ if __name__ == '__main__':
                     ])
                 
                 # update tau
-                tau *= 2
+                tau = round(tau * 2, 5)
             # update rho
-            rho += 0.2
+            rho = round(rho + 0.2, 1)
+
+        rho = 1
+        tau = 0
+        sub_logdir = f'{logdir}/{task[:-5]}/r{1}_t{0}/'
+        for i in range(5):
+            if local_search == None:
+                filename = f'{sub_logdir}{task[:-5]}_{algorithm}_{popsize}_{time.strftime("%m%d%H%M")}_{rho}_{tau}'
+            else:
+                filename = f'{sub_logdir}{task[:-5]}_{algorithm}_{local_search}_{popsize}_{time.strftime("%m%d%H%M")}_{rho}_{tau}'
+            print(f'[{i}/5]')
+            subprocess.run([
+                "python",
+                "Search.py",
+                "-A", algorithm,
+                "-s", sim_step,
+                "-e", evo_step,
+                "-d", sub_logdir,
+                "--popsize", popsize,
+                "--numprocs", numprocs,
+                "--rho", str(rho),
+                "--tau", str(tau),
+                "--filename", filename,
+                f"log/evogym_world/T_world/{task}",
+                "basicrobot",
+            ])
