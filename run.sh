d80fe5a2909c042b32652ef7981128678b0a4f3f
Milo-91 <milolin9112@gmail.com>
2026-07-17 23:08:44 +0900

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-for i in {1..10}; do python Search.py -A GA+ES -L ES -e 10000 -d log/20260706/Robust/ObstacleTraverser-v1T/GA+ES/ --popsize 100 --mu 20 --lamb 5 --mutation_size 2 --numprocs 16 --strong_evaluation world/evogym_world/T_world/ObstacleTraverser-v1T.json  basicrobot; done
-for i in {1..10}; do python Search.py -A GA+ES -L ES -e 10000 -d log/20260706/Robust/Climber-v0T/GA+ES/ --popsize 100 --mu 20 --lamb 5 --mutation_size 2 --numprocs 16 --strong_evaluation world/evogym_world/T_world/Climber-v0T.json  basicrobot; done
+for i in {1..10}; do python Search.py -A GA -e 10000 -d log/20260717/Robust/ObstacleTraverser-v1T/GA/ --popsize 100 --numprocs 16 --strong_evaluation world/evogym_world/T_world/ObstacleTraverser-v1T.json  basicrobot; done
+for i in {1..10}; do python Search.py -A GA -e 10000 -d log/20260717/Robust/Climber-v0T/GA/ --popsize 100 --numprocs 16 --strong_evaluation world/evogym_world/T_world/Climber-v0T.json  basicrobot; done
+for i in {1..10}; do python Search.py -A GA -e 10000 -d log/20260717/Robust/ObstacleTraverser-v0T/GA/ --popsize 100 --numprocs 16 --strong_evaluation world/evogym_world/T_world/ObstacleTraverser-v0T.json  basicrobot; done
