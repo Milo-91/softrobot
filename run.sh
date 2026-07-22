8b61b0d5c0b46176cd3febf4d5dac325ea67f5d0
Milo-91 <milolin9112@gmail.com>
2026-07-22 10:52:53 +0900

    run.sh debug

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # GA robust
-for i in {1..5}; do python Search.py -A GA -e 5000 -d log/20260721/robust/Hurdler-v0T/GA/ --popsize 100 --numprocs 32 --strong_evluation world/evogym_world/T_world/Hurdler-v0T.json  basicrobot; done
-for i in {1..5}; do python Search.py -A GA -e 5000 -d log/20260721/robust/UpStepper-v0T/GA/ --popsize 100 --numprocs 32 --strong_evluation world/evogym_world/T_world/UpStepper-v0T.json  basicrobot; done
+for i in {1..5}; do python Search.py -A GA -e 5000 -d log/20260721/robust/Hurdler-v0T/GA/ --popsize 100 --numprocs 32 --strong_evaluation world/evogym_world/T_world/Hurdler-v0T.json  basicrobot; done
+for i in {1..5}; do python Search.py -A GA -e 5000 -d log/20260721/robust/UpStepper-v0T/GA/ --popsize 100 --numprocs 32 --strong_evaluation world/evogym_world/T_world/UpStepper-v0T.json  basicrobot; done
